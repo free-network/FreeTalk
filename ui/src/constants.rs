85a1cd6c8b39f53cba013b6cafb0d8cb6ee73c4d
Maciej Krüger <mkg20001@gmail.com>
2026-02-26 15:01:40 +0100

    ci: fix

--- ui/src/constants.rs
+++ ui/src/constants.rs
@@ -6,6 +6,6 @@ pub const KEY_VERSION_PREFIX: &str = "river:v1";
 // These are the authoritative versions that match deployed contracts
 pub const BOARD_CONTRACT_WASM: &[u8] = include_bytes!("../public/contracts/board_contract.wasm");
 
-pub const CHAT_DELEGATE_WASM: &[u8] = include_bytes!("../public/contracts/chat_delegate.wasm");
+pub const CHAT_DELEGATE_WASM: &[u8] = include_bytes!("../public/contracts/freetalk_delegate.wasm");
 
 // pub const board_CONTRACT_CODE_HASH: CodeHash = CodeHash::from_code(BOARD_CONTRACT_WASM);
